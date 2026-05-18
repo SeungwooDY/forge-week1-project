02022456edfba05868d1431dc3b58fee104993a6
shrihanvijay <shrihanvijay06@gmail.com>
2026-05-18 13:22:05 -0400

    added navigation bar and updated app.jsx. ready to push

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import './App.css'
-
 import { Routes, Route } from 'react-router-dom'
-
 import Navbar from './components/Navbar'
 
 import Home from './pages/Home'
@@ -9,7 +6,7 @@ import Dashboard from './pages/Dashboard'
 import Classes from './pages/Classes'
 import ClassPage from './pages/ClassPage'
 import Students from './pages/Students'
-import Teachers from './pages/TeacherDirectory'
+import TeacherDirectory from './pages/TeacherDirectory'
 import Calendar from './pages/Calendar'
 import TeacherDashboard from './pages/TeacherDashboard'
 
@@ -24,7 +21,7 @@ function App() {
                 <Route path="/classes" element={<Classes />} />
                 <Route path="/classes/:classId" element={<ClassPage />} />
                 <Route path="/students" element={<Students />} />
-                <Route path="/teachers" element={<TeacherDirectory />} />
+                <Route path="/teacherdirectory" element={<TeacherDirectory />} />
                 <Route path="/calendar" element={<Calendar />} />
                 <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
             </Routes>
