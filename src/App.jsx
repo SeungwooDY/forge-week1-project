2a65b44455d42aa45e2585c9dcba4baaac43b2f8
Anushka1010 <136544048+Anushka1010@users.noreply.github.com>
2026-05-20 16:38:16 -0400

    Fixed duplicated router config error  by replacing routers with outlet in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
 
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
-import Classes from "./pages/Classes";
-import ClassPage from "./pages/ClassPage";
-import Students from "./pages/Students";
-import TeacherDirectory from "./pages/TeacherDirectory";
-import Calendar from "./pages/Calendar";
-
 export default function App() {
     return (
         <>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/classes" element={<Classes />} />
-                <Route path="/classes/:classId" element={<ClassPage />} />
-                <Route path="/students" element={<Students />} />
-                <Route
-                    path="/teacherdirectory"
-                    element={<TeacherDirectory />}
-                />
-                <Route path="/calendar" element={<Calendar />} />
-            </Routes>
+            <main>
+                <Outlet />
+            </main>
         </>
     );
 }
